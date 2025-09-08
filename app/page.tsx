8eb4acf64068f515a0096ff5ee6c694c157ffc24
lijinhai255 <18854109500@163.com>
2025-09-08 09:21:14 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your Startup, <br /> Connect width Entrpreneurs{" "}
+      </h1>
+    </section>
+  );
+}
